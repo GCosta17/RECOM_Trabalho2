748a905e633b2cced474e4de9db9a887e5e11cab
Costa <1211599@isep.ipp.pt>
2024-04-17 15:07:18 +0100

    1st

--- src/RFreception.java
+++ src/RFreception.java
@@ -1,12 +1,11 @@
-public class Main {
-    public static void main(String[] args) {
+public class RFreception {
 
+    private void inDoorSimulator() {
         int floor = 14;
         float doubleWallPlaster = 3.40F;
         int hollowBrick = 8;
         int massiveWall = 13;
         int window = 6;
-
 
 
 
