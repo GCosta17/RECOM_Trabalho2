cba27d077d2695ce4a35874bd5c425982812decb
Costa <1211599@isep.ipp.pt>
2024-04-17 15:23:04 +0100

    2nd

--- src/RFreception.java
+++ src/RFreception.java
@@ -1,13 +1,33 @@
+import java.util.Scanner;
+
 public class RFreception {
 
-    private void inDoorSimulator() {
+    public void inDoorSimulator() {
         int floor = 14;
         float doubleWallPlaster = 3.40F;
         int hollowBrick = 8;
         int massiveWall = 13;
         int window = 6;
 
+        int numberOfWalls;
+        int numberOfFloors;
+        int numberOfDoubleWallPlaster;
+        int numberOfHollowBrick;
+        int numberOfWindows;
 
+        float transmitedPower = 0F;
+        float frequency = 0F;
+        float gain = 0F;
+        float distanceFromAP = 0F;
+
+    }
+
+    private void UI extends inDoorSimulator () {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Introduce your frequency: ");
+        frequency = scanner.nextLine();
 
     }
 }
